a722c823986d7e45bca86e21957df5e4b5441b8a
Omprakash Choudhary <omseervi098@gmail.com>
2023-01-03 10:50:00 +0530

    removing unused code

--- workers/verify_email_worker.js
+++ workers/verify_email_worker.js
@@ -1,6 +1,12 @@
 const Queue = require("bull");
 const verifyMailer = require("../mailers/verifyemail_mailer");
-const verifyEmailQueue = new Queue("verifyemail", "redis://127.0.0.1:6379");
+const verifyEmailQueue = new Queue("verifyemail", {
+  redis: {
+    port: env.redis_port,
+    host: env.redis_url,
+    password: env.redis_password,
+  },
+});
 //run parallelly
 verifyEmailQueue.process(10, function (job, done) {
   verifyMailer.verifyEmailMail(job.data.email, job.data.token);
